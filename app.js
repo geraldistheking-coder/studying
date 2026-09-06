d9ccde06afd330cff93e311868649a7af72f1976
skylines_rgood <geraldistheking@gmail.com>
2026-09-06 09:51:50 -0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const categoryColors = {
 };
 
 const games = [
-  ["10 Minutes Till Dawn", "games/10minutes till dawn/index.html", "Action", null, "local"],
+  ["10 Minutes Till Dawn", "games/10minutes-till-dawn/index.html", "Action", null, "local"],
   ["2048", "2048", "Puzzle"],
   ["2048 Multitask", "2048-multitask", "Puzzle"],
   ["Achievement Unlocked", "achievementunlocked", "Platform"],
