b9a5083c02a94cf74da9884dbe5103d834db61d8
skylines_rgood <geraldistheking@gmail.com>
2026-09-06 12:19:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const games = [
   ["A Dark Room", "adarkroom", "Strategy"],
   ["Alien Hominid", "alienhominid", "Action"],
   ["Align 4", "align-4", "Strategy"],
+  ["Among Us", "games/amongus/index.html", "Strategy", null, "local"],
   ["Avalanche", "avalanche", "Arcade"],
   ["Black Knight", "blackknight", "Action"],
   ["Bloons TD", "bloonstd", "Strategy"],
